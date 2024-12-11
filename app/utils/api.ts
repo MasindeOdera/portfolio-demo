a60a70f2109093f286da2531fe6bad8e953068f4
Masinde Odera <masindeodera@gmail.com>
2024-12-11 21:28:27 +0100

    Improved strictness for TypeScript types

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-export const fetchProjects = async () => {
+export type Project = {
+  id?: number; // id may be optional when adding a new project
+  project: string;
+  image: string;
+  description: string;
+};
+
+export const fetchProjects = async (): Promise<Project[]> => {
   const response = await axios.get('https://67530214f3754fcea7ba71d6.mockapi.io/api/v1/portfolio');
   return response.data;
 };
 
-export const addProject = async (project) => {
+export const addProject = async (project: Project): Promise<Project> => {
   const response = await axios.post('https://67530214f3754fcea7ba71d6.mockapi.io/api/v1/portfolio', project);
   return response.data;
 };
